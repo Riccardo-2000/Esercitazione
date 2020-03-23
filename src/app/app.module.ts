a013d0ae99ccd65b2e2766c1109a383aae4ddca7
Riccardo-2000 <you@example.comrizzini.riccardo.marconi@gmail.com>
2020-03-23 10:13:57 +0100

    creati componenti base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { HomeComponent } from './component/home/home.component';
+import { GameListComponent } from './component/game-list/game-list.component';
+import { GameDetailComponent } from './component/game-detail/game-detail.component';
+import { EditGameComponent } from './component/edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    GameListComponent,
+    GameDetailComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
